2be20e135a7768fb2f7965226b8fc6dc067fda86
raspberry-pi <raspberry-pi@kakao.com>
2023-11-18 21:45:02 +0900

    Implement chasing the player

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -5,6 +5,8 @@ var is_moving = false
 
 @onready var tile_map = $%PoliceManager
 @onready var sprite = $Sprite
+@onready var animation_player = $AnimationPlayer
+var prev_dir = Vector2i.DOWN
 
 func input_direction():
 	var dir = Vector2i.ZERO
@@ -24,17 +26,30 @@ func player_turn():
 	var dir = input_direction()
 	if dir == Vector2i.ZERO:
 		return
+	prev_dir = dir
 	var cur_map_pos = get_map_position()
 	
 	if not is_obstacle(cur_map_pos + dir):
 		move(dir)
+	
 
 	_next_stage()
 
+func animate_non_moving():
+	if prev_dir == Vector2i.DOWN:
+		animation_player.play("DOWN")
+	elif prev_dir == Vector2i.UP:
+		animation_player.play("UP")
+	else:
+		animation_player.play("side")
+		sprite.flip_h = prev_dir.x > 0
+	
+
 func _physics_process(delta):
 	if is_moving  == false:
+		animate_non_moving()
 		return
-		
+	animation_player.play("walking")
 	if global_position == sprite.global_position:
 		is_moving = false
 		return
@@ -64,9 +79,7 @@ func get_map_position():
 func is_obstacle(pos: Vector2i):
 	return tile_map.is_occupied(pos)
 
-
 func _on_main_scene_onchange_state(state):
-	print(state)
 	if state == Constant.STATE_PLAYER:
 		is_turn = true
 	else:
