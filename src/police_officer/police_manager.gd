8e1f81232bc0a618b3faea8b17aa4e3adc1f693a
raspberry-pi <raspberry-pi@kakao.com>
2023-11-18 13:42:23 +0900

    Implement polices random moving

--- src/police_officer/police_manager.gd
+++ src/police_officer/police_manager.gd
@@ -4,7 +4,15 @@ var PoliceClass = load("res://police_officer/police_officer.gd")
 
 const POLICE_LAYER = 0
 
+var player = null
 var polices = []
+
+var rng = RandomNumberGenerator.new()
+
+var tiles = []
+
+func _ready():
+	rng.seed = 42
 
 func _on_main_scene_onchange_state(state):
 	if state == Constant.STATE_INIT:
@@ -22,13 +30,37 @@ func init_polices():
 	
 func recognize():
 	# TODO: is in light in player
-	get_parent().emit_signal("next_state")
+	_next_stage()
 
 func move_polices():
-	print(polices)
+	for p in polices:
+		# print(p)
+		var _max = 2 if not p.is_obstacle_in_front(self) else 1
+		var idx = rng.randi_range(0,_max)
+		
+		match idx:
+			0:
+				p.turn(true)
+			1:
+				p.turn(false)
+			2:
+				p.move_forward(self)
+		
 	# TODO: move polices
+	_next_stage()
 
+func move_cell(from: Vector2i, to:Vector2i):
+	var idx = tiles.find(from)
+	tiles.remove_at(idx)
+	tiles.append(to)
+	
+func is_occupied(pos: Vector2i):
+	return pos in tiles
 
 func _on_child_entered_tree(node:Node2D):
 	if node.get_script() == PoliceClass:
 		polices.append(node)
+		tiles.append(local_to_map(node.position))
+
+func _next_stage():
+	get_parent().emit_signal("next_state")
